4f9da5bce44fe3076bcdde65adf469f0b63986a8
tadayosi <tadayosi@33c96664-5241-0410-8970-4798b019ccea>
2009-01-03 06:52:30 +0000

    [maven-release-plugin]  copy for tag ouobpo-tools-amazonchecker-0.2

--- ouvroir/ouvroir-mule/src/main/java/org/ouobpo/ouvroir/mule/hello/HelloService.java
+++
@@ -1,9 +0,0 @@
-package org.ouobpo.ouvroir.mule.hello;
-
-public class HelloService {
-  private String fHello = "こんにちは、";
-
-  public String hello(String caller) {
-    return fHello + caller;
-  }
-}
