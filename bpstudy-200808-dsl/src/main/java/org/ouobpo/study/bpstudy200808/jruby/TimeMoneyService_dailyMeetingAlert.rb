3d56e15512a5bccc5538c148d950160c1a1e663c
tadayosi <tadayosi@33c96664-5241-0410-8970-4798b019ccea>
2009-01-22 11:38:11 +0000

    [maven-release-plugin]  copy for tag ouobpo-tools-amazonchecker-0.2.2

--- bpstudy-200808-dsl/src/main/java/org/ouobpo/study/bpstudy200808/jruby/TimeMoneyService_dailyMeetingAlert.rb
+++
@@ -1,8 +0,0 @@
-# Daily stand-up meeting at 10:00am each work day. (We work in Honolulu, of course.)
-# Notify 5 minutes before meeting starts.
-# Derive the TimePoint at which I should notify on April 19 2006.
-
-meeting_time.
-on(day).
-as_time_point($timeZoneFactory.timeZone).
-minus(duration)
