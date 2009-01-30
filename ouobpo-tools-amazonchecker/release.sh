314ccee539919ab657be59b8989542288b800d11
tadayosi <tadayosi@33c96664-5241-0410-8970-4798b019ccea>
2009-01-30 17:43:18 +0000

    improved release script.

--- ouobpo-tools-amazonchecker/release.sh
+++ ouobpo-tools-amazonchecker/release.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+if [ -z $1 ];
+then
+	echo "enter a version to release."
+	echo "> $0 <version>"
+	exit
+fi
+
 mvn release:perform -DconnectionUrl=scm:svn:https://ouobpo.googlecode.com/svn/tags/ouobpo-tools-amazonchecker-$1
